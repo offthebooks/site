2700450e7e540061a9acfc8790b4fa022a04666e
Chuck Skoda <charles.skoda@gmail.com>
2024-08-28 00:27:31 -0700

    More work on blog formatting.

--- blog/build.js
+++ blog/build.js
@@ -1,17 +1,96 @@
 const fs = require('fs')
 
+const rootUrl = 'https://offthebooks.games/blog'
 const itemsDir = './blog/items/'
+const postDir = './blog/post/'
 const unpublished = 'UNPUBLISHED'
+const tabCharacter = '  '
 
 const args = new Set(process.argv.slice(2).map((s) => s.toLowerCase()))
 const preview = args.has('preview')
 
+const temp = `<a href="/"><h1>Off The Books</h1></a>
+
+<h2>Blog</h2>
+
+<a href="./feed.rss">RSS Feed</a>
+
+<p>
+  Welcome to the Off The Books Blog. Here you'll find posts on the video game industry,
+  development, and related anecdotes.
+</p>
+
+<article>
+  <a href=""
+    ><span class="title">Post Title &rarr;</span>
+    <span class="date"
+      ><script>
+        document.write(new Date().toDateString())
+      </script></span
+    ></a
+  >
+  <blockquote>This is the pull quote from the post.</blockquote>
+</article>`
+
+function template({ title, description, image, url, content }) {
+  const joinStr = `\n${tabCharacter}${tabCharacter}`
+  const tabbedContent = `${content.split('\n').join(joinStr)}`
+  const copyright = `copyright ${new Date().getFullYear()}`
+
+  return `<html>
+  <head>
+    <title>${title}</title>
+    <meta
+      name="description"
+      content="${description}" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+    <meta property="og:title" content="${title}" />
+    <meta property="og:url" content="${url}" />
+    <meta property="og:image" content="${image ?? '/img/logo.jpg'}" />
+    <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+    <link rel="apple-touch-icon" href="/img/logo.jpg" />
+    <link href="/css/blog.css" rel="stylesheet" />
+  </head>
+  <body>
+    <a href="/"><h1>Off The Books</h1></a>
+    ${tabbedContent}
+    <footer>${copyright}</footer>
+  </body>
+</html>
+`
+}
+
+// Automatically wraps any lines in p tags,
+// unless we are inside a formatted block:
+const blocks = new Map({
+  '```': 'code',
+  '>>>': 'blockquote'
+})
+
 function markupLines(lines) {
+  let block = null
   lines = lines.flatMap((line) => {
     const text = line.trim()
     if (!text.length) return []
 
-    return `<p>${line}</p>`
+    if (blocks.has(line)) {
+      const tag = blocks.get(line)
+
+      if (block && block !== tag) {
+        throw new Error('Cannot nest blocks.')
+      }
+
+      // Close or open a block
+      if (block) {
+        block = null
+        return `</${tag}>`
+      } else {
+        block = tag
+        return `<${tag}>`
+      }
+    } else {
+      return block ? line : `<p>${line}</p>`
+    }
   })
 
   return lines.join('\n')
@@ -19,6 +98,14 @@ function markupLines(lines) {
 
 function generatePages(posts) {
   console.log(`Generating ${posts.length} pages`)
+  posts.forEach(({ title, slug, date, image, content }) => {
+    content = `<div><h2>${title}</h2><span class="date">${date}</span></div>\n${content}`
+    const url = `${rootUrl}/post/${slug}`
+    title = `${title} - Off The Books`
+    description = `${title} Blog Post`
+    const html = template({ title, description, image, url, content })
+    fs.writeFileSync(`${postDir}${slug}.html`, html)
+  })
 }
 
 function generateIndex(posts) {
@@ -33,7 +120,13 @@ function build() {
   const posts = fs.readdirSync(itemsDir).map((fname) => {
     const path = `${itemsDir}${fname}`
     const file = fs.readFileSync(path, 'utf8')
-    const [title, slug, publishDate, ...lines] = file.split('\n')
+    const lines = file.split('\n')
+
+    const meta = {}
+    while (lines[0].contains(':')) {
+      const [key, ...rest] = lines.shift().split(':')
+      meta[key] = rest.join(':')
+    }
 
     const date = unpublished
 
@@ -45,7 +138,7 @@ function build() {
       fs.writeFileSync(path, timestampedFile)
     }
 
-    return { title, slug, date, html: markupLines(lines) }
+    return { title, slug, date, content: markupLines(lines) }
   })
 
   if (preview) {
